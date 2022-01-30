fc02d15c2fe6935427ebd4f28c48380c59f09748
NickFarEast <shramko.dt@gmail.com>
2022-01-30 16:46:25 +1000

    выполнен шаг #3

--- function.py
+++ function.py
@@ -6,8 +6,11 @@ CANDIDATES_FILE_PATH = "candidates.json"
 SETTINGS_FILE_PATH = "settings.json"
 
 
-
 def get_candidates():
+    """
+    Функция для получения списка кандидатов из json-файла.
+    :return: словари с информацией о  кандидатах
+    """
     if isfile(CANDIDATES_FILE_PATH):
         with open(CANDIDATES_FILE_PATH, encoding="utf-8") as file:
             return json.load(file)
@@ -17,6 +20,10 @@ def get_candidates():
 
 
 def get_settings():
+    """
+    Функция для получения словаря с настройками из json-файла.
+    :return: словарь с настройками
+    """
     if isfile(SETTINGS_FILE_PATH):
         with open(SETTINGS_FILE_PATH) as file:
             return json.load(file)
@@ -26,9 +33,12 @@ def get_settings():
 
 
 def get_candidate_by_id(x):
-
+    """
+    Функция для извлечения информации о кандидате согласно введенному номеру id.
+    :param x: id кандидата
+    :return: возвращает словарь с информацией о кандидате согласно id
+    """
     candidates = get_candidates()
     for candidate in candidates:
         if candidate.get("id") == x:
             return candidate
-
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from function import get_settings, get_candidate_by_id
+from function import get_settings, get_candidate_by_id,get_candidates
 
 settings = get_settings()
 
@@ -19,12 +19,24 @@ def page_index():
 def page_candidate(x):
     candidate = get_candidate_by_id(x)
 
-    return f"""
-           <h1>{candidate["name"]}</h1>
-           <p>{candidate["position"]}</p>
-           <img src="{candidate["picture"]}" width=200/>
-           <p>{candidate["skills"]}</p>
-           """
+    page_content = f"""
+    <h1>{candidate["name"]}</h1>
+    <p>{candidate["position"]}</p>
+    <img src="{candidate["picture"]}" width=200/>
+    <p>{candidate["skills"]}</p>
+    """
+    return page_content
 
+@app.route('/list/')
+def page_list():
+
+    candidates = get_candidates()
+    page_content = "<h1>Все кандидаты</h1>"
+
+    for candidate in candidates:
+        page_content += f"""
+        <p><a href="/candidate/{candidate["id"]}"> {candidate["name"]}</a></p>
+        """
+    return page_content
 
 app.run()
