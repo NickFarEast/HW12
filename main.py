d4781f5f1b124cfc44ece4060ebe0615c48caf1c
NickFarEast <shramko.dt@gmail.com>
2022-01-30 14:58:36 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import json
+from os.path import isfile
+from flask import Flask, render_template
+
+CANDIDATES_FILE_PATH = "candidates.json"
+SETTINGS_FILE_PATH = "settings.json"
+
+
+def get_candidates():
+    if isfile(CANDIDATES_FILE_PATH):
+        with open(CANDIDATES_FILE_PATH) as file:
+            return json.load(file)
+    else:
+        print("Файл не найден")
+        return {}
+
+
+def get_settings():
+    if isfile(SETTINGS_FILE_PATH):
+        with open(SETTINGS_FILE_PATH) as file:
+            return json.load(file)
+    else:
+        print("Файл не найден")
+        return {}
+
+
+app = Flask(__name__)
+
+candidates = get_candidates()
+settings = get_settings()
+
+
+@app.route('/')
+def page_index():
+    return render_template("index.html", **settings)
+
+@app.route('candidate/<int:x>')
+def page_candidate(x):
+
+    return render_template("candidate.html", candidates=candidates)
+
+
+app.run()
