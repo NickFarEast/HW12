0bacb4a7b7c02bb6d0495ee574376ec5a5bd9bbb
NickFarEast <shramko.dt@gmail.com>
2022-01-31 19:35:42 +1000

    добавлены шаблоны, завершен шаг 3

--- main.py
+++ main.py
@@ -1,46 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 from function import get_settings, get_candidate_by_id, get_candidates, search_candidate_by_name, search_candidate_by_skill
 
-settings = get_settings()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def page_index():
-    online = settings.get("online")
-    if online:
-        return "Приложение работает"
-    else:
-        return "Приложение не работает"
+    settings = get_settings()
+    return render_template("index.html", **settings)
 
 
 @app.route('/candidate/<int:x>')
 def page_candidate(x):
     candidate = get_candidate_by_id(x)
 
-    page_content = f"""
-    <h1>{candidate["name"]}</h1>
-    <p>{candidate["position"]}</p>
-    <img src="{candidate["picture"]}" width=200/>
-    <p>{candidate["skills"]}</p>
-    """
-    return page_content
+    return render_template("candidate.html", **candidate)
 
 
 @app.route('/list/')
 def page_list():
     candidates = get_candidates()
-    page_content = "<h1>Все кандидаты</h1>"
-
-    for candidate in candidates:
-        page_content += f"""
-        <p><a href="/candidate/{candidate["id"]}"> {candidate["name"]}</a></p>
-        """
-    return page_content
+    return render_template("list.html", candidates=candidates)
 
 
-@app.route("/search/")
+@app.route("/search")
 def page_search_by_name():
     name = request.args.get("name", "")
     candidates = search_candidate_by_name(name)
